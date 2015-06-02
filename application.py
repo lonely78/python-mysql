b67e6506103bcbc32ccc6ca9f07119019369724b
pig <ckjoker78@gmail.com>
2015-06-02 18:52:20 +0800

    update ch

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def hello_world():
   storage = Storage()
   storage.populate()
   score = storage.score()
-  return "工作人员辛苦了！, %d!" % score
+  return "Are you OK?  %d!" % score
 
 class Storage():
   def __init__(self):
